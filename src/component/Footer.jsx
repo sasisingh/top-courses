bd7bb4018c96de9bef79e07c7061a8cbd9a40e1f
sasisingh <sasisingh6387@gmail.com>
2023-06-12 22:42:08 +0530

    initial commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='container-fluid bg-light'>
+<div className='row'>
+  <b style={{textAlign:"center"}}>@ 2023 Image-Gallery</b>
+    </div>    
+    </div>
+  )
+}
+
+export default Footer
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../component/Header'
+import Footer from '../component/Footer'
+import Card from '../component/Card'
+
+function Home() {
+  return (
+    <div>
+    <Header/>
+    <Card/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Home
