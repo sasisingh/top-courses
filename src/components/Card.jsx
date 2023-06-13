fc4867a8543177b57234ea14514b602f64cdcd4b
sasisingh <sasisingh6387@gmail.com>
2023-06-14 02:37:58 +0530

    inital commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 const Card = ({ selectImage }) => {
   return (
-    <div className="row" style={{marginBottom:"70px"}} >
+    <div className="row" style={{ marginBottom: "70px" }} >
       {
-        selectImage == "" && <b className="text-center mt-5">Image Not Found</b>
+        selectImage === "" && <b className="text-center mt-5">Image Not Found</b>
       }
       {
-        selectImage && selectImage.map((image) => {
+        selectImage && selectImage.map((item) => {
           return (
             <div className="col-sm-6 col-lg-4 text-center">
-              <img src={image} alt="picture" height="250px" width="300px" className="m-4" />
+              <img src={item} alt="picture" height="250px" width="300px" className="m-4" />
             </div>
           )
         }
