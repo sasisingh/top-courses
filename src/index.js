72c6bf1bf6718ecae3ae59920eac7b224c266857
sasisingh <sasisingh6387@gmail.com>
2023-11-23 16:48:33 +0530

    fouth commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ToastContainer,toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
